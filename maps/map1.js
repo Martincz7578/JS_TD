ca0864d58b641e244d35e719c4a37b2d3b999ecc
Martincz7578 <martinek@rychly.cz>
2025-09-20 17:08:34 +0200

    Last minute changes

--- maps/map1.js
+++ maps/map1.js
@@ -1,15 +1,28 @@
 import { setStackerPos } from "../JS/towers/stacker.js";
+import { revertChoice } from "../JS/gejm.js";
 
 let turretPositions = [
     {x: 2, y: 2},
     {x: 3, y: 3},
-    {x: 5, y: 5}
+    {x: 5, y: 5},
+    {x: 6, y: 2},
+    {x: 7, y: 4},
+    {x: 8, y: 3}
 ]
+
+let placedTowers = {
+    stacker: false
+}
 
 export function setTower(position, tower) {
     switch(tower) {
         case "stacker":
-            setStackerPos(turretPositions[position - 1].x * 50, turretPositions[position - 1].y * 50);
+            if (!placedTowers.stacker) {
+                setStackerPos(turretPositions[position - 1].x * 50, turretPositions[position - 1].y * 50);
+                placedTowers.stacker = true;
+            }else{
+                revertChoice(document.getElementById(`slot${position}`), "None");
+            }
             break;
         case "None":
             break;
